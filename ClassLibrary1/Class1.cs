450fecd96f5649d546b70e92b13f4481f9e22a03
Ostap4ik777 <137522973+Ostap4i@users.noreply.github.com>
2024-08-28 11:16:46 +0300

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static void Print(Object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+    public static class ClassLibrary1
+    {
+        public static Int32 Max(List<Int32> numbers)
+        {
+            return numbers.Max();
+        }
+
+        public static Int32 Min(List<Int32> numbers)
+        {
+            return numbers.Min();
+        }
+
+        public static Int32 Sum(List<Int32> numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
